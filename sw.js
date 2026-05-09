7e1285eccc5b31dfdd1b9e133b46c0ac758ef12e
ruslan000000 <38035352+ruslan000000@users.noreply.github.com>
2026-05-10 01:39:18 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = "fitness-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
